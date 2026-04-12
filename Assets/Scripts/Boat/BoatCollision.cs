f48f252af9710b0a7a9c94ab93daefc3da2f9577
Miilena <mtmartinezc@uoc.edu>
2026-04-13 00:29:25 +0200

    Se realiza el sprite del barco y se ajusta colisiones

--- Assets/Scripts/Boat/BoatCollision.cs
+++ Assets/Scripts/Boat/BoatCollision.cs
@@ -4,10 +4,26 @@ public class BoatCollision : MonoBehaviour
 {
     public BoatScoreController scoreController;
 
+    public AudioSource audioSource;
+    public AudioClip crashSound;
+
+    private bool hasCollided = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (hasCollided) return;
+
         if (other.CompareTag("Iceberg"))
         {
+            hasCollided = true;
+
+            // reproducir sonido
+            if (audioSource != null && crashSound != null)
+            {
+                audioSource.PlayOneShot(crashSound);
+            }
+
+            // perder
             scoreController.GameOver();
         }
     }

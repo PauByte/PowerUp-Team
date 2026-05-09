1765fc45c0b4b53e36d3603e7731b4ad494420a1
EmanuelRodriguez <jrodriguezapa@uoc.edu>
2026-05-09 10:41:16 +0200

    Musica y efecto puestos

--- Assets/Scripts/ARRace/PlayerCollisionAR.cs
+++ Assets/Scripts/ARRace/PlayerCollisionAR.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerCollisionAR : MonoBehaviour
 {
     private bool gameOver = false;
 
     private ARRaceGameManager gameManager;
+    private AudioSource hitSound;
 
     void Start()
     {
-        gameManager = FindFirstObjectByType<ARRaceGameManager>();
+        gameManager =
+            FindFirstObjectByType<ARRaceGameManager>();
+
+        hitSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,7 +24,22 @@ public class PlayerCollisionAR : MonoBehaviour
         {
             gameOver = true;
 
-            gameManager.GameOver();
+            StartCoroutine(CrashSequence());
         }
+    }
+
+    IEnumerator CrashSequence()
+    {
+        // sonido choque
+        if (hitSound != null)
+        {
+            hitSound.Play();
+        }
+
+        // pequeña espera
+        yield return new WaitForSeconds(0.3f);
+
+        // game over
+        gameManager.GameOver();
     }
 }
